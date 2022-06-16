90d8cd9a689ad9a66d3b910b1b96f2ceae3e1840
igori572 <igorilic572@gmail.com>
2022-06-16 19:51:16 +0200

    Rad sa Oglasima

---
+++ rad-sa-Oglasima/oglasi.js
@@ -0,0 +1,50 @@
+const fs=require('fs');
+const { sveKnjige } = require('radknjige-modul');
+const PATH="oglasi.json";
+
+const kategorije=[
+    "automobil",
+    "stan",
+    "alat"
+];
+
+//kategorija,datum,cena,tekst,oznaka,email
+let procitaj=()=>{
+    let oglasi=fs.readFileSync(PATH,(err,data)=>{
+        if(err) throw err;
+            return data;
+    });
+    return JSON.parse(oglasi);
+};
+
+let snimiOglase=(data)=>{
+    fs.writeFileSync(PATH,JSON.stringify(data));
+}
+
+exports.sviOglasi=()=>{
+    return procitaj();
+}
+
+exports.dodajOglas=(noviOglas)=>{
+    let id=1;
+    let oglasi=this.sviOglasi();
+    if(oglasi.length>0){
+        id=oglasi[oglasi.length-1].id+1;
+    }
+    noviOglas.id=id;
+    oglasi.push(noviOglas);
+    snimiOglase(oglasi);
+}
+
+exports.getOglas=(id)=>{
+    return this.sviOglasi().find(o=>o.id==id);
+}
+
+exports.obrisiOglas=(id)=>{
+    snimiOglase(this.sviOglasi().filter(oglas=>oglas.id!=id));
+}
+
+exports.filtrirajOglase=()=>{
+
+}
+
