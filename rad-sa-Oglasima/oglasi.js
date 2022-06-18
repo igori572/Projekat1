26b83fabf6e6343347911df3c2975d1df2e42f0a
igori572 <igorilic572@gmail.com>
2022-06-18 13:44:03 +0200

    Oglasi NODE

--- rad-sa-Oglasima/oglasi.js
+++ rad-sa-Oglasima/oglasi.js
@@ -1,12 +1,5 @@
 const fs=require('fs');
-const { sveKnjige } = require('radknjige-modul');
 const PATH="oglasi.json";
-
-const kategorije=[
-    "automobil",
-    "stan",
-    "alat"
-];
 
 //kategorija,datum,cena,tekst,oznaka,email
 let procitaj=()=>{
@@ -44,37 +37,15 @@ exports.obrisiOglas=(id)=>{
     snimiOglase(this.sviOglasi().filter(oglas=>oglas.id!=id));
 }
 
-exports.izmeniOglas=(id,oglas)=>{
+exports.izmeniOglas=()=>{
     
-    const brisanjeResp=this.obrisiOglas(id)
-    if(brisanjeResp.err) return brisanjeResp;
-
-    let oglasi=this.sviOglasi()
-    oglas.id=id
-    oglasi.push(oglas);
+    this.oglasi[this.oglasi.findIndex(o=>o.id==oglas.id)]=oglas
     snimiOglase(oglasi);
 }
 
 
 exports.filtrirajOglaseKategorija=(kategorija)=>{
-    return this.sviOglasi().filter(oglas=>oglas.kategorija==kategorija);
-}
-exports.filtrirajOglaseOznaka=(oznaka)=>{
-    return this.sviOglasi().filter(oglas=>oglas.oznaka.toLowerCase().includes(oznaka.toLowerCase()))
-}
-exports.filtrirajOglaseCena=(cena)=>{
-    return this.sviOglasi().filter(oglas=>oglas.cena<=cena)
+    return this.sviOglasi().filter(oglas=>oglas.kategorija.toLoweCase().includes(kategorija.toLoweCase()));
 }
 
-exports.filtrirajOglase=(kategorija,cena,oznaka)=>{
-    let oglasi = procitaj();
-  if (kategorija) oglasi = oglasi.filter(o => o.kategorija == kategorija);
-  if (cena) oglasi = oglasi.filter(o => o.cena <= cena);
-  if (oznaka)
-    oglasi = oglasi.filter(o=>
-      o.oznaka.toLowerCase().includes(oznaka.toLowerCase())
-    );
-
-  return oglasi;
-}
 
